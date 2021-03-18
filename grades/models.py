911ac3324a9253e7ab0a7e568ee64b8b9339735e
ilyuhich <ilyuhich@gmail.com>
2021-03-18 04:47:01 +0300

    create Quarter model

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Quarter(models.Model):
+    """records for class and quarter"""
+    year = models.SmallIntegerField(verbose_name='Год обучения')
+    quarter = models.SmallIntegerField(verbose_name='Четверть')
+
+    def __str__(self):
+        return 'Класс: ' + str(self.year) + ', четверть: ' + str(self.quarter)
+
+    class Meta:
+        verbose_name = 'Четверть'
+        verbose_name_plural = 'Четверти'
