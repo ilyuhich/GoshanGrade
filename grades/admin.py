76416cc9a3a59e89f8f2b6041342ccdc33e41c17
ilyuhich <ilyuhich@gmail.com>
2021-03-18 05:07:43 +0300

    create Grades model

--- grades/admin.py
+++ grades/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Quarters, Subjects
+from .models import Quarters, Subjects, Grades
 
 # Register your models here.
 admin.site.register(Quarters)
+admin.site.register(Subjects)
+admin.site.register(Grades)
--- grades/models.py
+++ grades/models.py
@@ -25,3 +25,17 @@ class Subjects(models.Model):
     class Meta:
         verbose_name = 'Предмет'
         verbose_name_plural = 'Предметы'
+
+
+class Grades(models.Model):
+    grade_date = models.DateField(verbose_name='Дата')
+    grade_quarter = models.ForeignKey('Quarters', on_delete=models.PROTECT, verbose_name='четверть')
+    grade_subject = models.ForeignKey('Subjects', on_delete=models.PROTECT, verbose_name='предмет')
+    grade = models.SmallIntegerField(verbose_name='оценка')
+
+    def __str__(self):
+        return str(self.grade_date) + ' ' + self.grade_subject.name + ' ' + str(self.grade)
+
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
