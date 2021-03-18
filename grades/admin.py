edd4cd2545f6c1970dc09c06cc1ce5cc51bfa6eb
ilyuhich <ilyuhich@gmail.com>
2021-03-18 05:34:25 +0300

    Fetched from github

--- grades/admin.py
+++ grades/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Quarters, Subjects, Grades
 
 # Register your models here.
+admin.site.register(Quarters)
+admin.site.register(Subjects)
+admin.site.register(Grades)
--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Quarters(models.Model):
+    """records for class and quarter"""
+    year = models.SmallIntegerField(verbose_name='Год обучения')
+    quarter = models.SmallIntegerField(verbose_name='Четверть')
+
+    def __str__(self):
+        return 'Класс: ' + str(self.year) + ', четверть: ' + str(self.quarter)
+
+    class Meta:
+        verbose_name = 'Четверть'
+        verbose_name_plural = 'Четверти'
+
+
+class Subjects(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Предмет'
+        verbose_name_plural = 'Предметы'
+
+
+class Grades(models.Model):
+    grade_date = models.DateField(verbose_name='Дата')
+    grade_quarter = models.ForeignKey('Quarters', on_delete=models.PROTECT, verbose_name='четверть')
+    grade_subject = models.ForeignKey('Subjects', on_delete=models.PROTECT, verbose_name='предмет')
+    grade = models.SmallIntegerField(verbose_name='оценка')
+
+    def __str__(self):
+        return str(self.grade_date) + ' ' + self.grade_subject.name + ' ' + str(self.grade)
+
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
