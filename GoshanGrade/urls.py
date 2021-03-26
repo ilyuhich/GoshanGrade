417314f185f19ed0bddbe1b0bd8e1d8693b19832
ilyuhich <ilyuhich@gmail.com>
2021-03-27 02:18:08 +0300

    add new class based view AllGrades plus nedeed imports

--- GoshanGrade/urls.py
+++ GoshanGrade/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gg/', include('grades.urls'))
+    path('gg/', include('grades.urls')),
+
 ]
--- grades/views.py
+++ grades/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Grades
 
 
 # Create your views here.
 def hello_message(request):
     return HttpResponse("Hello from my soul, World!!!")
+
+
+class AllGrades(ListView):
+    model = Grades
